706d15cee7e373ec1b646d5ff782657e6a6cfdcf
unknown <ukrpurojekuto@gmail.com>
2025-07-17 14:50:24 +0530

    removed

--- src/transcriptionDisabled/src/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building insta-downloader-gui...
-pyinstaller --name "insta-downloader-gui" ^
-  --windowed ^
-  --icon=favicon.ico ^
-  main.py
-pause
--- src/transcriptionEnabled/src/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building insta-downloader-gui...
-pyinstaller --name "insta-downloader-gui" ^
-  --windowed ^
-  --onefile ^
-  --icon=favicon.ico ^
-  main.py
-pause
